c72c1344145ee891af32680726100e2f5583a26f
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-11-22 22:34:50 +0700

    feat: added endpoint to the soap

--- ws/src/main/java/com/binotify/App.java
+++ ws/src/main/java/com/binotify/App.java
@@ -1,6 +1,12 @@
 package com.binotify;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javax.xml.ws.Endpoint;
+
+import com.binotify.helpers.DBConnector;
+import com.binotify.services.*;
 
 /**
  * Hello world!
@@ -8,8 +14,33 @@ import javax.xml.ws.Endpoint;
  */
 public class App 
 {
+    static final String host = "0.0.0.0";
+    static final int port = 8042;
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try(
+            Connection conn = DBConnector.getConnection()
+        ) {
+            RequestReviewService.conn = conn;
+            StatusCheckService.conn = conn;
+            SubscriptionRequestService.conn = conn;
+
+            String baseUrl = String.format("http://%s:%d/", App.host, App.port);
+
+            Endpoint.publish(baseUrl + "admin", new RequestReviewService());
+            System.out.println("Endpoint for admin started");
+
+            Endpoint.publish(baseUrl + "check", new StatusCheckService());
+            System.out.println("Endpoint for check started");
+
+            Endpoint.publish(baseUrl + "request", new SubscriptionRequestService());
+            System.out.println("Endpoint for request started");
+            
+            while(true) {
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
