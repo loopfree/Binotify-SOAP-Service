62fcda5a8aabf39668c2df0b3ffb463a1634c08f
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-11-22 22:28:56 +0700

    feat & refactor: request-review-service
    feat:
    - menambahkan menerima
    - menambahkan menolak
    
    refactor:
    - logging dipindah ke interface

--- ws/src/main/java/com/binotify/services/RequestReviewService.java
+++ ws/src/main/java/com/binotify/services/RequestReviewService.java
@@ -1,37 +1,61 @@
 package com.binotify.services;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.time.Instant;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 
 import javax.xml.ws.WebServiceContext;
-import javax.xml.ws.handler.MessageContext;
-
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.xml.ws.developer.JAXWSProperties;
 
 import javax.annotation.Resource;
 
-import com.binotify.helpers.Logging;
+import com.binotify.helpers.Logger;
+import com.binotify.model.Subscription;
 
-public class RequestReviewService {
+@WebService
+public class RequestReviewService implements Logger{
     @Resource
     WebServiceContext wsContext;
     
+    public static Connection conn;
+    
     @WebMethod
-    public ArrayList<HashMap<String, String>> getSubscriptionRequests() {
-        MessageContext msgContext = wsContext.getMessageContext();
-        HttpExchange req = (HttpExchange) msgContext.get(JAXWSProperties.HTTP_EXCHANGE);
+    public Subscription[] getSubscriptionRequests() {
+        Connection conn = RequestReviewService.conn;
+        String query = "SELECT * FROM Subscription WHERE status = 'PENDING';";
 
-        String ip = String.format("%s", req.getRemoteAddress());
-        String endpoint = String.format("%s", req.getRequestURI());
-        Instant timestamp = Instant.now();
+        ArrayList<Subscription> result = new ArrayList<>();
 
-        return new ArrayList<>();
+        try(
+            PreparedStatement pStatement = conn.prepareStatement(query)
+        ) {
+            ArrayList<Subscription> temp = new ArrayList<>();
+            
+            ResultSet resultSet = pStatement.executeQuery();
+
+            while(resultSet.next()) {
+                Subscription s = new Subscription();
+                s.setCreatorId(resultSet.getInt("creator_id"));
+                s.setSubscriberId(resultSet.getInt("subscriber_id"));
+                s.setStatus(resultSet.getString("status"));
+                
+                temp.add(s);
+            }
+
+            result = temp;
+
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        Log(wsContext, conn, "Admin requests PENDING subscription");
+
+        return result.toArray(new Subscription[0]); 
     }
 
     @WebMethod
@@ -39,6 +63,25 @@ public class RequestReviewService {
         @WebParam(name = "subscriberId") Integer subscriberId,
         @WebParam(name = "creatorId") Integer creatorId
     ) {
+        Connection conn = RequestReviewService.conn;
+
+        String query = "UPDATE Subscription SET status = 'ACCEPTED' WHERE creator_id = ? AND subscriber_id = ?;";
+
+        try(
+            PreparedStatement pStatement = conn.prepareStatement(query)
+        ) {
+
+            pStatement.setInt(1, creatorId);
+            pStatement.setInt(2, subscriberId);
+
+            pStatement.execute();
+
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        Log(wsContext, conn, "Admin approves subscription request");
+
         return;
     }
 
@@ -47,6 +90,25 @@ public class RequestReviewService {
         @WebParam(name = "subscriberId") Integer subscriberId,
         @WebParam(name = "creatorId") Integer creatorId
     ) {
+        Connection conn = RequestReviewService.conn;
+
+        String query = "UPDATE Subscription SET status = 'REJECTED' WHERE creator_id = ? AND subscriber_id = ?;";
+
+        try(
+            PreparedStatement pStatement = conn.prepareStatement(query)
+        ) {
+
+            pStatement.setInt(1, creatorId);
+            pStatement.setInt(2, subscriberId);
+
+            pStatement.execute();
+
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        Log(wsContext, conn, "Admin declined subscription request");
+
         return;
     }
 }
